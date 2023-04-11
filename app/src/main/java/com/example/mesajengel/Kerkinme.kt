e4582f9eb1f4a2058a5d2e94f457041a5bbef510
Atherius28 <2022933001@ogr.cu.edu.tr>
2023-04-11 22:36:55 +0300

    Kerkinme Gonderisi 0.0.1

---
+++ app/src/main/java/com/example/mesajengel/Kerkinme.kt
@@ -0,0 +1,11 @@
+package com.example.mesajengel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Kerkinme : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kerkinme)
+    }
+}
